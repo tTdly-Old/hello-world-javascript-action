983b6ad1692a9ae144c82d702ac373f49874ea06
tTdly-Old <92563872+tTdly-Old@users.noreply.github.com>
2023-03-27 16:01:20 +0800

    consoleContext

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  const githubContext = JSON.stringify(github.context);
+  console.log(githubContext);
 } catch (error) {
   core.setFailed(error.message);
 }
